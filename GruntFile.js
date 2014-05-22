474dfaff5eda3e573dacfdfb22d26c6639524888
Aliaksei Tuzik <konanmentor@gmail.com>
2014-05-22 20:09:59 +0300

    bundle.js location changed

--- GruntFile.js
+++ GruntFile.js
@@ -9,7 +9,7 @@ module.exports = function (grunt) {
 		uglify: {
 			dev : {
 				files : {
-					"src/app/bundle.min.js" : "src/app/bundle.js"
+					"src/bundle.min.js" : "src/bundle.js"
 				}
 			}
 		},
@@ -17,7 +17,7 @@ module.exports = function (grunt) {
 			js : {
 				src : ["bower_components/angular/angular.js", "bower_components/angular-route/angular-route.js", "bower_components/jquery/dist/jquery.js",
 					"src/common/items.js", "src/app/list/list.js", "src/app/newItem/newItem.js", "src/app/app.js"],
-				dest : "src/app/bundle.js"
+				dest : "src/bundle.js"
 			}
 		}
 	});
--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-	  'https://ajax.googleapis.com/ajax/libs/angularjs/1.2.15/angular.js',
+	  'src/bundle.min.js',
       'test/**/*.spec.js'
     ],
 
